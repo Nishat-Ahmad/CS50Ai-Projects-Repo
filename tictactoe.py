168f2fedb9d7e9910d72d3afd539ec2eab47003b
Assassin-Droid210 <nishat.ahmed1200@gmail.com>
2024-08-05 18:02:58 +0500

    Wrote the whole tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -22,67 +23,152 @@ def player(board) -> str:
     """
     Returns player who has the next turn on a board.
     """
+    countX = 0
+    countO = 0
     
-    
-    # In the initial game state, X gets the first move. Subsequently, the player alternates with each additional move.
-    # Any return value is acceptable if a terminal board is provided as input (i.e., the game is already over).
-    raise NotImplementedError
+    for row in range(len(board)):
+        for column in range(len(board[row])):
+            if board[row][column] == X:
+                countX +=  1
+            elif board[row][column] == O:
+                countO += 1
+            
+    if countX > countO:
+        return O
+    else:
+        return X
 
-
-def actions(board) -> tuple:
+def actions(board) -> set:
     """
     Returns set of all possible actions (i, j) available on the board.
     """
+    allPossibleActions = set()
     
-    # Return any move if terminal board provided
-    # Each action is a tuple (x, y) legal move
-    raise NotImplementedError
-
+    for row in range(len(board)):
+        for column in range(len(board[0])):
+            if board[row][column] == EMPTY:
+                allPossibleActions.add((row, column))
+            
+            
+    return allPossibleActions
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
     
-    # If action not valid raise an error.
-    # Make a deep copy of the board so that minimax doesn't changes the board.
-    raise NotImplementedError
+    if action not in actions(board):
+        raise Exception("Not legal move")
 
+    row, column = action
+    newboard = copy.deepcopy(board)
+    
+    newboard[row][column] = player(board)
+    
+    return newboard
+    
+def row_check_winner(board, player):
+    for row in range(len(board)):
+        if board[row][0] == player and board[row][1] == player and board[row][2] == player:
+            return True
+    
+    return False
 
+def column_check_winner(board, player):
+    for column in range(3):
+        if board[0][column] == player and board[1][column] == player and board[2][column] == player:
+                return True
+    
+    return False
+
+def diagonal_check_winer(board, player):
+    if board[0][0] == player and board[1][1] == player and board[2][2] == player:
+        return True
+
+    if board[2][0] == player and board[1][1] == player and board[0][2] == player:
+        return True
+    
+    return False
+    
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
     
-    # Should return x if x has won and o if o has won
-    # If tie return None
-    raise NotImplementedError
-
+    if row_check_winner(board, X) or column_check_winner(board, X) or diagonal_check_winer(board, X):
+        return X
+    elif row_check_winner(board, O) or column_check_winner(board, O) or diagonal_check_winer(board, O):
+        return O
+    else:
+        return EMPTY
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
+    if winner(board) == X:
+        return True
+    elif winner(board) == O:
+        return True
     
-    # Shoud return True if someone won or the game is over
-    # Otherwise return false
-    raise NotImplementedError
-
+    for row in range(3):
+        for column in range(3):
+            if board[row][column] == None:
+                return False
+            
+    return True
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    
-    # Returns 1 if X won. returns -1 if O wins, and return 0 if draw
-    raise NotImplementedError
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
 
+def max_value(board):
+    v = -math.inf
+    
+    if terminal(board):
+        return utility(board)
+    for action in actions(board):
+        v = max(v, min_value(result(board, action)))
+    
+    return v
+
+def min_value(board):
+    v = math.inf
+    
+    if terminal(board):
+        return utility(board)
+    for action in actions(board):
+        v = min(v, max_value(result(board, action)))
+    
+    return v
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
+    plays = []
     
-    # should return (i, j) which is one of the allowable actions, & if multiple moves are optimal return any
-    # if Terminal board return None
-    raise NotImplementedError
+    if terminal(board):
+        return None
+    
+    elif player(board) == X:
+        
+        for action in actions(board):
+            plays.append([min_value(result(board, action)), action])
+            
+        return sorted(plays, key=lambda x : x[0], reverse=True)[0][1]
+    
+    elif player(board) == O:
+        plays = []
+        
+        for action in actions(board):
+            plays.append([min_value(result(board, action)), action])
+            
+        return sorted(plays, key=lambda x : x[0])[0][1]
